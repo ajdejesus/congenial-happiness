7656d4b4e3e301bfe7b0950c85fae11054706cd6
Cristian Lopez <cristianmlopez18@gmail.com>
2023-02-09 23:58:42 -0500

    Made Paddle.js a little more dynamic. working on index.js trying to see what can be done in terms of movement.

--- components/Paddle.js
+++ components/Paddle.js
@@ -4,30 +4,19 @@
 */
 
 export class Paddle {
-    constructor(canvas, x, y, width = 30, height = 200) {
-        this.canvas = canvas;
-        this.x = x;
-        this.y = y;
+    constructor(width, height, color, x, y, gravity) {
+        this.canvas = document.querySelector("#canvas");
+        this.c = canvas.getContext("2d");
         this.width = width;
         this.height = height;
-        this.c = this.canvas.getContext('2d');
-        this.setPosition();
-    }
-    setPosition() {
-        this.c.beginPath();
-        this.c.fillStyle = 'white';
+        this.x = x;
+        this.y = y;
+        this.c.fillStyle = color
+        this.gravity = gravity;
         this.c.fillRect(this.x, this.y, this.width, this.height);
     }
-    setControllers(upKey, downKey) {
-        this.upKey = upKey;
-        this.downKey = downKey;
     }
-    getControllers() {
-        return [this.upKey, this.downKey];
-    }
-    move(newY) {
-        this.c.clearRect(this.x, this.y, this.width, this.height);
-        this.y = newY;
-        this.setPosition();
-    }
-}
+      
+
+
+
--- index/Index.js
+++ index/Index.js
@@ -2,36 +2,42 @@
 * Index.js
 * @author Alejandro De Jesús
 */
-
 import { Paddle } from "../components/Paddle.js";
-import { appendChildren, setClass } from "../helpers/helpers.js";
-import { div, p } from "../helpers/htmlElements.js";
 
-export class Index {
-    constructor() {
-        this.root = document.getElementById('root');
-        this.canvas = document.querySelector('canvas');
-        this.c = this.canvas.getContext('2d');
-        this.setView();
+class Pong{
+    constructor(){
+        this.canvas = document.querySelector("#canvas");
+        this.c = canvas.getContext("2d");
+        this.scoreOne = 0;
+        this.scoreTwo = 0;
+        this.canvasSize();
+        this.elements();
+        this.displayScoreOne();
     }
-    setView() {
-        this.canvas.width = 1280;
+    canvasSize(){
         this.canvas.height = 720;
-        this.c.strokeStyle = 'white';
-        this.player1 = new Paddle(this.canvas, 100, 100);
-        this.player2 = new Paddle(this.canvas, 1180, 100);
-
-        this.player1.setControllers('w', 's');
-        this.player2.setControllers('ArrowUp', 'ArrowDown');
-
-        document.addEventListener('keydown', event => {
-            if (this.player1.getControllers().includes(event.key)) {
-                this.player1.move(event.key == this.player1.upKey ? this.player1.y - 10 : this.player1.y + 10);
-            } else if (this.player2.getControllers().includes(event.key)) {
-                this.player2.move(event.key == this.player2.upKey ? this.player2.y - 10 : this.player2.y + 10)
-            }
-        });
+        this.canvas.width = 1280;
+        
     }
+    elements(){
+        this.playerOne = new Paddle(20, 150, "white", 100, 250, 2);
+        this.playerTwo = new Paddle(20, 150, "white", 1100, 250, 2);
+        this.ball = new Paddle (15, 15, "red", 1280 /2, 720 /2, 2);
+    }
+    displayScoreOne(){
+        this.c.font = "18px Arial"
+        this.c.fillStyle = "white";
+        this.c.fillText(scoreOne, canvas.width/2 - 60, 30)
+    }
+    displayScoreTwo(){
+        this.c.font = "18px Arial"
+        this.c.fillStyle = "white";
+        this.c.fillText(scoreOne, canvas.width/2 + 60, 30)
+    }
+  
 }
 
-window.onload = () => new Index();
+
+window.onload = () => new Pong();
+
+
